3863074d860e0d8ad2cf5adca6194152aa95260d
v-os <voidef@gmail.com>
2016-03-28 21:30:37 +0300

    teams A and B

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     int scoreTeamA = 0;
+    int scoreTeamB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +22,13 @@ public class MainActivity extends AppCompatActivity {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
     }
-
+    /**
+     * Displays the given score for Team B.
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
     public void add3PointsTeamA(View view) {
         scoreTeamA += 3;
         displayForTeamA(scoreTeamA);
@@ -35,5 +42,19 @@ public class MainActivity extends AppCompatActivity {
     public void add1PointTeamA(View view) {
         scoreTeamA += 1;
         displayForTeamA(scoreTeamA);
+    }
+    public void add3PointsTeamB(View view) {
+        scoreTeamB += 3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void add2PointsTeamB(View view) {
+        scoreTeamB += 2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void add1PointTeamB(View view) {
+        scoreTeamB += 1;
+        displayForTeamB(scoreTeamB);
     }
 }
