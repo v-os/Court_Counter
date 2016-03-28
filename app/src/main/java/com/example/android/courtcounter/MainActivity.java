f09a7fae87c3c896e0f368bcf6d4089782c85176
v-os <voidef@gmail.com>
2016-03-28 21:02:21 +0300

    team A only

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -1,13 +1,39 @@
 package com.example.android.courtcounter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    int scoreTeamA = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void add3PointsTeamA(View view) {
+        scoreTeamA += 3;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void add2PointsTeamA(View view) {
+        scoreTeamA += 2;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void add1PointTeamA(View view) {
+        scoreTeamA += 1;
+        displayForTeamA(scoreTeamA);
     }
 }
